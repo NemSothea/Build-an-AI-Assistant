c59446c53a3463a591cb376ac0e810122b834ed1
sothea007 <nemsothea13@gmail.com>
2024-12-10 11:35:32 +0700

    Initial Commit

---
+++ AIExapenseTracker/AIExapenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIExapenseTrackerApp.swift
+//  AIExapenseTracker
+//
+//  Created by sothea007 on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AIExapenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIExapenseTrackerTests/AIExapenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIExapenseTrackerTests.swift
+//  AIExapenseTrackerTests
+//
+//  Created by sothea007 on 10/12/24.
+//
+
+import Testing
+@testable import AIExapenseTracker
+
+struct AIExapenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
