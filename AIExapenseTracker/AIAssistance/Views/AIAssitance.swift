4123bafcbcafab9d74acc3906b4cd0d12d3222cf
sothea007 <nemsothea13@gmail.com>
2024-12-20 15:10:26 +0700

    - update

---
+++ AIExapenseTracker/AIAssistance/Views/AIAssitance.swift
@@ -0,0 +1,18 @@
+//
+//  AIAssitance.swift
+//  AIExapenseTracker
+//
+//  Created by sothea007 on 20/12/24.
+//
+
+import SwiftUI
+
+struct AIAssitance: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AIAssitance()
+}
